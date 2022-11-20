5e31bd1f4a68f1a5b6d50a9d070b874b35b9d969
Kim Jong IL <nijin39@gmail.com>
2022-11-20 21:06:31 +0900

    typo 수정

--- src/main/java/com/tandem6/nopostore/store/mapper/StoreMapper.java
+++ src/main/java/com/tandem6/nopostore/store/mapper/StoreMapper.java
@@ -17,7 +17,7 @@ public interface StoreMapper {
             select b.MANAGEMENT_NO, b.LICENSE_ISSUE_DATE, b.STORE_NAME, b.LOCATION_ADDRESS, b.BUSINESS_TYPE, a.Ranking
             from
                  (select RESTAURANT_ID,AVG(STAR_RATING) AS Ranking
-                  from star_rating group by RESTAURANT_ID order by Ranking desc) a, restarant
+                  from star_rating group by RESTAURANT_ID order by Ranking desc) a, restarant b
             where a.RESTAURANT_ID = b.MANAGEMENT_NO
                 AND b.BUSINESS_STATUS_NAME = '영업/정상'
             order by b.LICENSE_ISSUE_DATE asc limit 100;
