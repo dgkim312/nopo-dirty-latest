2bdaee9d881a8d0aaeaae0e7f31e303c2c414ad1
Kim Jong IL <nijin39@gmail.com>
2022-11-20 19:21:04 +0900

    SQS Example CODE

--- src/main/java/com/tandem6/nopostore/config/SqsConfig.java
+++ src/main/java/com/tandem6/nopostore/config/SqsConfig.java
@@ -2,12 +2,8 @@ package com.tandem6.nopostore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
-
-import java.net.URI;
 
 @Configuration
 public class SqsConfig {
