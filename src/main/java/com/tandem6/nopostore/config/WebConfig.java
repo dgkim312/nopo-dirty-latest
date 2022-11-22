a543226a4c6a5914795c8428a16b0b1dc55efa44
Kim Jong IL <nijin39@gmail.com>
2022-11-22 17:40:02 +0900

    webconfig

--- src/main/java/com/tandem6/nopostore/config/WebConfig.java
+++ src/main/java/com/tandem6/nopostore/config/WebConfig.java
@@ -3,29 +3,45 @@ package com.tandem6.nopostore.config;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
-import com.amazonaws.xray.plugins.EC2Plugin;
 import com.amazonaws.xray.strategy.sampling.CentralizedSamplingStrategy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.ResourceUtils;
 
 import javax.servlet.Filter;
+import java.io.FileNotFoundException;
 import java.net.URL;
 
+@Slf4j
 @Profile("dev")
 @Configuration
 public class WebConfig {
-    static {
-        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin());
+    private static final String SAMPLING_RULE_JSON = "classpath:/sampling-rules.json";
 
-        URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-        builder.withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
+    static {
+
+        URL ruleFile = null;
+        try {
+            ruleFile = ResourceUtils.getURL(SAMPLING_RULE_JSON);
+        } catch (FileNotFoundException e) {
+            log.error("sampling rule cannot load for aws xray - {}", e.getMessage());
+        }
+        log.debug("sampling rule load from {} for aws xray", ruleFile.getPath());
+
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard()
+                .withDefaultPlugins()
+                .withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
+
         AWSXRay.setGlobalRecorder(builder.build());
+        log.debug("aws xray recorder was setted globally.");
     }
 
     @Bean
     public Filter TracingFilter() {
-        return new AWSXRayServletFilter("JoTest");
+        log.debug("The segment name for aws xray tracking has been set to {}.", "nopo");
+        return new AWSXRayServletFilter("nopo");
     }
 }
 
