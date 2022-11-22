ccb0aef1684d2f01fa6553893e76848433c5c60e
Kim Jong IL <nijin39@gmail.com>
2022-11-22 17:48:05 +0900

    webconfig

--- src/main/java/com/tandem6/nopostore/config/WebConfig.java
+++ src/main/java/com/tandem6/nopostore/config/WebConfig.java
@@ -1,18 +1,13 @@
 package com.tandem6.nopostore.config;
 
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.AWSXRayRecorderBuilder;
+
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
-import com.amazonaws.xray.strategy.sampling.CentralizedSamplingStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.util.ResourceUtils;
 
 import javax.servlet.Filter;
-import java.io.FileNotFoundException;
-import java.net.URL;
 
 @Slf4j
 @Profile("dev")
