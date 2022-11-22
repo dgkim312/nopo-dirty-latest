ec4a9a281ae4f2227a6b544e800293ff85576c14
Kim Jong IL <nijin39@gmail.com>
2022-11-22 20:03:48 +0900

    package

---
+++ src/main/java/com/tandem6/nopostore/config/AWSXRayHikariConfig.java
@@ -0,0 +1,36 @@
+package com.tandem6.nopostore.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AWSXRayHikariConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    //DataSourceProperties will convert `spring.datasource.url` property to hikari's jdbcUrl property
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "hikariDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")//set hikari specific properties
+    public HikariDataSource hikariDataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean(name = "dataSource")
+    @Primary
+    public DataSource dataSource(HikariDataSource hikariDataSource) {
+        //wrap the spring ds into xray tracing ds
+        return new AWSXRayTracingDataSource(hikariDataSource);
+    }
+
+}
